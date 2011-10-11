7472ffaf6cebc6aef83e4365b2b38d4a0f796869
Scott Barr <scottjbarr@gmail.com>
2011-10-11 16:39:41 +0800

    Added Summary.for(symbol)
    Version bump to 0.0.3

---
+++ lib/scottjbarr-nasdaq/summary.rb
@@ -0,0 +1,67 @@
+module Nasdaq
+
+  # The Summary class collects the summary information that NASDAQ provides.
+  # This data is recalculated often. I am currently only interested in the
+  # shares_outstanding value, so most of the data is ignored.
+  class Summary < Hashie::Dash
+
+    include Base
+
+    property :symbol
+    property :stock_exchange
+    property :shares_outstanding
+
+    def self.path(symbol)
+      "/symbol/#{symbol.downcase}"
+    end
+
+    def self.uri(symbol)
+      URI.parse("http://#{SERVER}#{path(symbol)}")
+    end
+
+    def self.for(symbol)
+      response = get(uri(symbol))
+      parse(response.body)
+    end
+
+    def self.parse(body)
+      doc = Nokogiri::HTML.parse(body)
+
+      # parse the symbol and stock exchange
+      symbol_details = doc.css("#sq_symbol-info")
+      symbol = symbol_details.children.first.content.split(" ").first
+      stock_exchange = symbol_details.children.last.content.split(" ").last
+
+      # get the data table
+      table_data = get_table_data(doc.css("table[@class='datatable1_qn']"))
+      shares_outstanding = table_data[11].gsub(/[^-.0-9]/, '').to_i
+
+      hash = {
+        :symbol => symbol,
+        :stock_exchange => stock_exchange,
+        :shares_outstanding => shares_outstanding
+      }
+
+      Summary.new(hash)
+    end
+
+    def self.get_table_data(table)
+      data = []
+
+      table.children.each_with_index do |tr, i|
+        tr.children.each_with_index do |child, j|
+          if child.name == 'td'
+            data << child.children.first.content.gsub("\n", "").strip
+          end
+        end
+      end
+
+      # delete the "labels" from the data
+      data.delete_if { |d| d[-1, 1] == ":" }
+
+      data
+    end
+
+  end
+
+end
---
+++ test/test_summary.rb
@@ -0,0 +1,30 @@
+require 'test/test_helper'
+
+class TestSummary < Test::Unit::TestCase
+
+  include Nasdaq
+
+  def setup
+  end
+
+  def test_should_have_path_for_symbol
+    assert_equal "/symbol/mo", Summary.path("MO")
+  end
+
+  def test_should_have_uri_for_symbol
+    uri = Summary.uri("MO")
+    assert_equal URI::HTTP, uri.class
+    assert_equal "http://#{SERVER}/symbol/mo", uri.to_s
+  end
+
+  def test_should_get_summary_for_symbol
+    stub_get(Summary.uri("MO"), "mo_quote_and_summary.html")
+
+    summary = Summary.for("MO")
+
+    assert_equal 2071234000, summary.shares_outstanding
+    assert_equal "MO", summary.symbol
+    assert_equal "NYSE", summary.stock_exchange
+  end
+
+end
